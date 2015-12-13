97520a5ccc0f95e6ab77a36b52d14605e4f819de
MrWolvetech <MrWolvetech@users.noreply.github.com>
2015-12-13 20:21:44 +0100

    Update Fragments.java

--- app/src/main/java/net/ilexiconn/hipster/fragment/Fragments.java
+++ app/src/main/java/net/ilexiconn/hipster/fragment/Fragments.java
@@ -7,16 +7,16 @@ import net.ilexiconn.hipster.fragment.main.SettingsFragment;
 import net.ilexiconn.hipster.fragment.main.TimetableFragment;
 
 public enum Fragments {
-    DASHBOARD(R.id.nav_dashboard, new DashboardFragment(), R.drawable.ic_class_black_24dp),
-    GRADES(R.id.nav_grades, new GradesFragment(), R.drawable.ic_done_all_black_24dp),
-    TIMETABLE(R.id.nav_timetable, new TimetableFragment(), R.drawable.ic_watch_later_black_24dp),
-    SETTINGS(R.id.nav_settings, new SettingsFragment(), R.drawable.ic_settings_black_24dp);
+    DASHBOARD(R.id.nav_dashboard,  DashboardFragment.class, R.drawable.ic_class_black_24dp),
+    GRADES(R.id.nav_grades, GradesFragment.class, R.drawable.ic_done_all_black_24dp),
+    TIMETABLE(R.id.nav_timetable, TimetableFragment.class, R.drawable.ic_watch_later_black_24dp),
+    SETTINGS(R.id.nav_settings, SettingsFragment.class, R.drawable.ic_settings_black_24dp);
 
     private int id;
-    private IFragment fragment;
+    private Class fragment;
     private int icon;
 
-    Fragments(int id, IFragment fragment, int icon) {
+    Fragments(int id, Class fragment, int icon) {
         this.id = id;
         this.fragment = fragment;
         this.icon = icon;
@@ -27,7 +27,11 @@ public enum Fragments {
     }
 
     public IFragment getFragment() {
-        return fragment;
+        try {
+            return (IFragment) fragment.newInstance();
+        }catch (Exception e){
+            return null;
+        }
     }
 
     public int getIcon() {
